106acb7c39f9d3b504004f08cd7e16c7ed39fdc7
zhangweiwang <zhangweiwang@pwrd.cn>
2017-09-13 20:10:33 +0800

    my modify

--- src/main/java/com/github/wycm/GeettestCrawler.java
+++ src/main/java/com/github/wycm/GeettestCrawler.java
@@ -32,17 +32,20 @@ import java.util.regex.Pattern;
 public class GeettestCrawler {
     private static String basePath = "src/main/resources/";
     private static String FULL_IMAGE_NAME = "full-image";
-    private static String BG_IMAGE_NAME = "bg-image";
-    private static int[][] moveArray = new int[52][2];
+    private static String divClass = "mCaptchaImgDiv";
+    private static String picExt = ".png";
+    private static int[][] moveArray = new int[40][2];
     private static boolean moveArrayInit = false;
-    private static String INDEX_URL = "https://passport.feng.com/?r=user/register";
+    private static String INDEX_URL = "http://captchas.wanmei.com/demo/mCaptcha";
     private static WebDriver driver;
 
     static {
-        System.setProperty("webdriver.chrome.driver", "D:/dev/selenium/chromedriver_V2.30/chromedriver_win32/chromedriver.exe");
-        if (!System.getProperty("os.name").toLowerCase().contains("windows")){
-            System.setProperty("webdriver.chrome.driver", "/Users/wangyang/workspace/selenium/chromedriver_V2.30/chromedriver");
-        }
+        System.setProperty("webdriver.chrome.driver", "D:/workspace/chromedriver_win32/chromedriver.exe");
+        //非windows操作系统选择如下路径
+//        if (!System.getProperty("os.name").toLowerCase().contains("windows")){
+//            System.setProperty("webdriver.chrome.driver", "D:/workspace/chromedriver_win32/chromedriver.exe");
+//        }
+        System.out.println(System.getProperty("os.name"));
         driver = new ChromeDriver();
     }
 
@@ -62,32 +65,43 @@ public class GeettestCrawler {
         //设置input参数
         driver.get(INDEX_URL);
 
-        //通过[class=gt_slider_knob gt_show]
-        By moveBtn = By.cssSelector(".gt_slider_knob.gt_show");
-        waitForLoad(driver, moveBtn);
-        WebElement moveElemet = driver.findElement(moveBtn);
-        int i = 0;
-        while (i++ < 15){
-            int distance = getMoveDistance(driver);
-            move(driver, moveElemet, distance - 6);
-            By gtTypeBy = By.cssSelector(".gt_info_type");
-            By gtInfoBy = By.cssSelector(".gt_info_content");
-            waitForLoad(driver, gtTypeBy);
-            waitForLoad(driver, gtInfoBy);
-            String gtType = driver.findElement(gtTypeBy).getText();
-            String gtInfo = driver.findElement(gtInfoBy).getText();
-            System.out.println(gtType + "---" + gtInfo);
-            /**
-             * 再来一次：
-             * 验证失败：
-             */
-            if(!gtType.equals("再来一次:") && !gtType.equals("验证失败:")){
-                Thread.sleep(4000);
-                System.out.println(driver);
-                break;
-            }
-            Thread.sleep(4000);
-        }
+        //FIXME 通过[class=gt_slider_knob gt_show]
+        By btnBy = By.cssSelector(".mCaptchaSlideBorder");
+        waitForLoad(driver, btnBy);
+        WebElement moveButton = driver.findElement(btnBy);
+        By picDiv = By.cssSelector("."+divClass);
+        waitForLoad(driver, picDiv);
+        //获得原始图片
+        String originImgName= getOriginImg(driver);
+        //拉动滑块
+        startDragBlock(originImgName,moveButton);
+        //FIXME for test
+        distance  = 200;
+
+//        int i = 0;
+//        while (i++ < 15){
+//            int distance = getMoveDistance(driver);
+//            //FIXME for test
+//            distance  = 200;
+//            move(driver, moveElemet, distance - 6);
+//            By gtTypeBy = By.cssSelector(".gt_info_type");
+//            By gtInfoBy = By.cssSelector(".gt_info_content");
+//            waitForLoad(driver, gtTypeBy);
+//            waitForLoad(driver, gtInfoBy);
+//            String gtType = driver.findElement(gtTypeBy).getText();
+//            String gtInfo = driver.findElement(gtInfoBy).getText();
+//            System.out.println(gtType + "---" + gtInfo);
+//            /**
+//             * 再来一次：
+//             * 验证失败：
+//             */
+//            if(!gtType.equals("再来一次:") && !gtType.equals("验证失败:")){
+//                Thread.sleep(4000);
+//                System.out.println(driver);
+//                break;
+//            }
+//            Thread.sleep(4000);
+//        }
     }
 
     /**
@@ -98,36 +112,53 @@ public class GeettestCrawler {
      * @throws InterruptedException
      */
     public static void move(WebDriver driver, WebElement element, int distance) throws InterruptedException {
-        int xDis = distance + 11;
-        System.out.println("应平移距离：" + xDis);
-        int moveX = new Random().nextInt(8) - 5;
-        int moveY = 1;
+        printLocation(element);
+        System.out.println("应平移距离：" + distance);
         Actions actions = new Actions(driver);
+        //按下鼠标左键
         new Actions(driver).clickAndHold(element).perform();
-        Thread.sleep(200);
-        printLocation(element);
-        actions.moveToElement(element, moveX, moveY).perform();
-        System.out.println(moveX + "--" + moveY);
-        printLocation(element);
-        for (int i = 0; i < 22; i++){
-            int s = 10;
-            if (i % 2 == 0){
-                s = -10;
-            }
-            actions.moveToElement(element, s, 1).perform();
-//            printLocation(element);
-            Thread.sleep(new Random().nextInt(100) + 150);
-        }
 
-        System.out.println(xDis + "--" + 1);
-        actions.moveByOffset(xDis, 1).perform();
-        printLocation(element);
-        Thread.sleep(200);
+        int xMoveDistance = 0;
+        //每次移动的量
+        int xStep = 10;
+        while(xMoveDistance<distance){
+            if(xMoveDistance+xStep > distance){
+                xStep = distance - xMoveDistance;
+            }
+            xMoveDistance+=xStep;
+            actions.moveByOffset(xStep, 0).perform();
+            //打印控件位置
+            printLocation(element);
+            System.out.println("move distance = "+xMoveDistance);
+            //每0.5s移动一次
+            Thread.sleep(500);
+        }
+        //松开鼠标左键
         actions.release(element).perform();
+//        Thread.sleep(200);
+//        printLocation(element);
+//        actions.moveToElement(element, moveX, moveY).perform();
+//        System.out.println(moveX + "--" + moveY);
+        printLocation(element);
+//        for (int i = 0; i < 22; i++){
+//            int s = 10;
+//            if (i % 2 == 0){
+//                s = -10;
+//            }
+//            actions.moveToElement(element, s, 1).perform();
+////            printLocation(element);
+//            Thread.sleep(new Random().nextInt(100) + 150);
+//        }
+//
+//        System.out.println(xDis + "--" + 1);
+//        actions.moveByOffset(xDis, 1).perform();
+//        printLocation(element);
+//        Thread.sleep(200);
+
     }
     private static void printLocation(WebElement element){
         Point point  = element.getLocation();
-        System.out.println(point.toString());
+        System.out.println(element.getAttribute("class")+" position = "+point.toString());
     }
     /**
      * 等待元素加载，10s超时
@@ -147,39 +178,21 @@ public class GeettestCrawler {
     }
 
     /**
-     * 计算需要平移的距离
+     * 还原图片为未打乱状态
      * @param driver
      * @return
      * @throws IOException
      */
-    public static int getMoveDistance(WebDriver driver) throws IOException {
+    public static String getOriginImg(WebDriver driver) throws IOException {
         String pageSource = driver.getPageSource();
+        //获得图片URL
         String fullImageUrl = getFullImageUrl(pageSource);
-        FileUtils.copyURLToFile(new URL(fullImageUrl), new File(basePath + FULL_IMAGE_NAME + ".jpg"));
-        String getBgImageUrl = getBgImageUrl(pageSource);
-        FileUtils.copyURLToFile(new URL(getBgImageUrl), new File(basePath + BG_IMAGE_NAME + ".jpg"));
+        String initImageName = basePath + FULL_IMAGE_NAME + picExt;
+        //将URL图片资源保存至本地文件
+        FileUtils.copyURLToFile(new URL(fullImageUrl), new File(initImageName));
         initMoveArray(driver);
-        restoreImage(FULL_IMAGE_NAME);
-        restoreImage(BG_IMAGE_NAME);
-        BufferedImage fullBI = ImageIO.read(new File(basePath + "result/" + FULL_IMAGE_NAME + "result3.jpg"));
-        BufferedImage bgBI = ImageIO.read(new File(basePath + "result/" + BG_IMAGE_NAME + "result3.jpg"));
-        for (int i = 0; i < bgBI.getWidth(); i++){
-            for (int j = 0; j < bgBI.getHeight(); j++) {
-                int[] fullRgb = new int[3];
-                fullRgb[0] = (fullBI.getRGB(i, j)  & 0xff0000) >> 16;
-                fullRgb[1] = (fullBI.getRGB(i, j)  & 0xff00) >> 8;
-                fullRgb[2] = (fullBI.getRGB(i, j)  & 0xff);
-
-                int[] bgRgb = new int[3];
-                bgRgb[0] = (bgBI.getRGB(i, j)  & 0xff0000) >> 16;
-                bgRgb[1] = (bgBI.getRGB(i, j)  & 0xff00) >> 8;
-                bgRgb[2] = (bgBI.getRGB(i, j)  & 0xff);
-                if(difference(fullRgb, bgRgb) > 255){
-                    return i;
-                }
-            }
-        }
-        throw new RuntimeException("未找到需要平移的位置");
+        String finalImageName = restoreImage(FULL_IMAGE_NAME,2,20);
+        return finalImageName;
     }
     private static int difference(int[] a, int[] b){
         return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]) + Math.abs(a[2] - b[2]);
@@ -193,15 +206,17 @@ public class GeettestCrawler {
             return;
         }
         Document document = Jsoup.parse(driver.getPageSource());
-        Elements elements = document.select("[class=gt_cut_bg gt_show]").first().children();
+        Elements elements = document.select("[class="+divClass+"]");
         int i = 0;
         for(Element element : elements){
-            Pattern pattern = Pattern.compile(".*background-position: (.*?)px (.*?)px.*");
+            Pattern pattern = Pattern.compile(".*background-position: (-*\\d*)px (-*\\d*).*");
+
             Matcher matcher = pattern.matcher(element.toString());
             if (matcher.find()){
                 String width = matcher.group(1);
                 String height = matcher.group(2);
                 moveArray[i][0] = Integer.parseInt(width);
+                //FIXME is the i correct?
                 moveArray[i++][1] = Integer.parseInt(height);
             } else {
                 throw new RuntimeException("解析异常");
@@ -211,34 +226,38 @@ public class GeettestCrawler {
     }
     /**
      *还原图片
-     * @param type
+     * @param baseName
      */
-    private static void restoreImage(String type) throws IOException {
-        //把图片裁剪为2 * 26份
-        for(int i = 0; i < 52; i++){
-            cutPic(basePath + type +".jpg"
-                    ,basePath + "result/" + type + i + ".jpg", -moveArray[i][0], -moveArray[i][1], 10, 58);
+    private static String restoreImage(String baseName,int row, int col) throws IOException {
+        //把图片裁剪为row * col份
+        int cutNum = row*col;
+        for(int i = 0; i < cutNum; i++){
+            ImageUtils.cutPic(basePath + baseName +picExt
+                    ,basePath + "result/" + baseName + i + picExt, -moveArray[i][0], -moveArray[i][1], 13, 60);
         }
         //拼接图片
-        String[] b = new String[26];
-        for(int i = 0; i < 26; i++){
-            b[i] = String.format(basePath + "result/" + type + "%d.jpg", i);
+        String[] picColList = new String[col];//图片行向量
+        String[] picRowList = new String[row];//图片列向量
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                picColList[j] = String.format(basePath+"result/"+baseName+"%d"+picExt,j+i*col);
+            }
+            //生成目标文件名
+            picRowList[i] = String.format(basePath+"result/"+baseName+"row-%d"+picExt,i);
+            //对图片列作拼接
+            ImageUtils.jointImage(picColList,1,picRowList[i]);
         }
-        mergeImage(b, 1, basePath + "result/" + type + "result1.jpg");
-        //拼接图片
-        String[] c = new String[26];
-        for(int i = 0; i < 26; i++){
-            c[i] = String.format(basePath + "result/" + type + "%d.jpg", i + 26);
-        }
-        mergeImage(c, 1, basePath + "result/" + type + "result2.jpg");
-        mergeImage(new String[]{basePath + "result/" + type + "result1.jpg",
-                basePath + "result/" + type + "result2.jpg"}, 2, basePath + "result/" + type + "result3.jpg");
-        //删除产生的中间图片
-        for(int i = 0; i < 52; i++){
-            new File(basePath + "result/" + type + i + ".jpg").deleteOnExit();
-        }
-        new File(basePath + "result/" + type + "result1.jpg").deleteOnExit();
-        new File(basePath + "result/" + type + "result2.jpg").deleteOnExit();
+        String fileName = basePath+"result/"+baseName+"-final"+picExt;
+        //对图片行作拼接
+        ImageUtils.jointImage(picRowList,2,fileName);
+        return fileName;
+
+//        //删除产生的中间图片
+//        for(int i = 0; i < 52; i++){
+//            new File(basePath + "result/" + baseName + i + picExt).deleteOnExit();
+//        }
+//        new File(basePath + "result/" + baseName + "result1.jpg").deleteOnExit();
+//        new File(basePath + "result/" + baseName + "result2.jpg").deleteOnExit();
     }
     /**
      * 获取原始图url
@@ -248,136 +267,30 @@ public class GeettestCrawler {
     private static String getFullImageUrl(String pageSource){
         String url = null;
         Document document = Jsoup.parse(pageSource);
-        String style = document.select("[class=gt_cut_fullbg_slice]").first().attr("style");
-        Pattern pattern = Pattern.compile("url\\(\"(.*)\"\\)");
+//        System.out.println("Document = "+document);
+        String style = document.select("[class="+divClass+"]").first().attr("style");
+        Pattern pattern = Pattern.compile("url\\(.*\\)");
         Matcher matcher = pattern.matcher(style);
         if (matcher.find()){
-            url = matcher.group(1);
+            url = matcher.group(0);
         }
-        url = url.replace(".webp", ".jpg");
+        url = url.substring(4,url.lastIndexOf(')'));
         System.out.println(url);
         return url;
     }
+
+    private static void startDragBlock(String originImgName,WebElement moveBtn){
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(moveBtn);
+
+    }
+
+
     /**
-     * 获取带背景的url
-     * @param pageSource
+     * 确定拼图滑块的高度
      * @return
      */
-    private static String getBgImageUrl(String pageSource){
-        String url = null;
-        Document document = Jsoup.parse(pageSource);
-        String style = document.select(".gt_cut_bg_slice").first().attr("style");
-        Pattern pattern = Pattern.compile("url\\(\"(.*)\"\\)");
-        Matcher matcher = pattern.matcher(style);
-        if (matcher.find()){
-            url = matcher.group(1);
-        }
-        url = url.replace(".webp", ".jpg");
-        System.out.println(url);
-        return url;
-    }
-    public static boolean cutPic(String srcFile, String outFile, int x, int y,
-                                 int width, int height) {
-        FileInputStream is = null;
-        ImageInputStream iis = null;
-        try {
-            if (!new File(srcFile).exists()) {
-                return false;
-            }
-            is = new FileInputStream(srcFile);
-            String ext = srcFile.substring(srcFile.lastIndexOf(".") + 1);
-            Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName(ext);
-            ImageReader reader = it.next();
-            iis = ImageIO.createImageInputStream(is);
-            reader.setInput(iis, true);
-            ImageReadParam param = reader.getDefaultReadParam();
-            Rectangle rect = new Rectangle(x, y, width, height);
-            param.setSourceRegion(rect);
-            BufferedImage bi = reader.read(0, param);
-            File tempOutFile = new File(outFile);
-            if (!tempOutFile.exists()) {
-                tempOutFile.mkdirs();
-            }
-            ImageIO.write(bi, ext, new File(outFile));
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-                if (iis != null) {
-                    iis.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        }
-    }
-    /**
-     * 图片拼接 （注意：必须两张图片长宽一致哦）
-     * @param files 要拼接的文件列表
-     * @param type  1横向拼接，2 纵向拼接
-     * @param targetFile 输出文件
-     */
-    private static void mergeImage(String[] files, int type, String targetFile) {
-        int length = files.length;
-        File[] src = new File[length];
-        BufferedImage[] images = new BufferedImage[length];
-        int[][] ImageArrays = new int[length][];
-        for (int i = 0; i < length; i++) {
-            try {
-                src[i] = new File(files[i]);
-                images[i] = ImageIO.read(src[i]);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            int width = images[i].getWidth();
-            int height = images[i].getHeight();
-            ImageArrays[i] = new int[width * height];
-            ImageArrays[i] = images[i].getRGB(0, 0, width, height, ImageArrays[i], 0, width);
-        }
-        int newHeight = 0;
-        int newWidth = 0;
-        for (int i = 0; i < images.length; i++) {
-            // 横向
-            if (type == 1) {
-                newHeight = newHeight > images[i].getHeight() ? newHeight : images[i].getHeight();
-                newWidth += images[i].getWidth();
-            } else if (type == 2) {// 纵向
-                newWidth = newWidth > images[i].getWidth() ? newWidth : images[i].getWidth();
-                newHeight += images[i].getHeight();
-            }
-        }
-        if (type == 1 && newWidth < 1) {
-            return;
-        }
-        if (type == 2 && newHeight < 1) {
-            return;
-        }
-        // 生成新图片
-        try {
-            BufferedImage ImageNew = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
-            int height_i = 0;
-            int width_i = 0;
-            for (int i = 0; i < images.length; i++) {
-                if (type == 1) {
-                    ImageNew.setRGB(width_i, 0, images[i].getWidth(), newHeight, ImageArrays[i], 0,
-                            images[i].getWidth());
-                    width_i += images[i].getWidth();
-                } else if (type == 2) {
-                    ImageNew.setRGB(0, height_i, newWidth, images[i].getHeight(), ImageArrays[i], 0, newWidth);
-                    height_i += images[i].getHeight();
-                }
-            }
-            //输出想要的图片
-            ImageIO.write(ImageNew, targetFile.split("\\.")[1], new File(targetFile));
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private int findYposOfCubic(){
+//
+//    }
 }
---
+++ src/main/java/com/github/wycm/ImageUtils.java
@@ -0,0 +1,134 @@
+package com.github.wycm;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * @author WeiWang Zhang
+ * @date 2017/9/13.
+ * @time 14:50.
+ */
+public class ImageUtils {
+    /**
+     * 图片预剪切
+     * @param srcFile 输入图片源
+     * @param outFile 输出图片源
+     * @param x 剪切点x坐标
+     * @param y 剪切点y坐标
+     * @param width 剪切矩形宽度
+     * @param height 剪切矩形高度
+     * @return
+     */
+    public static BufferedImage cutPic(String srcFile, String outFile, int x, int y,
+                                 int width, int height) {
+        FileInputStream fis = null;
+        ImageInputStream iis = null;
+        try {
+            if (!new File(srcFile).exists()) {
+                return null;
+            }
+            fis = new FileInputStream(srcFile);
+            String ext = srcFile.substring(srcFile.lastIndexOf(".") + 1);
+            Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName(ext);
+            ImageReader reader = it.next();
+            iis = ImageIO.createImageInputStream(fis);
+            reader.setInput(iis, true);
+            ImageReadParam param = reader.getDefaultReadParam();
+            Rectangle rect = new Rectangle(x, y, width, height);
+            param.setSourceRegion(rect);
+            BufferedImage bi = reader.read(0, param);
+            File tempOutFile = new File(outFile);
+            if (!tempOutFile.exists()) {
+                tempOutFile.mkdirs();
+            }
+            ImageIO.write(bi, ext, new File(outFile));
+            return bi;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (iis != null) {
+                    iis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * 图片拼接 （注意：必须两张图片长宽一致哦）
+     * @param files 要拼接的文件列表
+     * @param type  1横向拼接，2 纵向拼接
+     * @param targetFile 输出文件
+     */
+    public static void jointImage(String[] files, int type, String targetFile) {
+        //获得要拼接的图片数量
+        int length = files.length;
+        File[] src = new File[length];
+        BufferedImage[] images = new BufferedImage[length];
+        int[][] ImageArrays = new int[length][];
+        for (int i = 0; i < length; i++) {
+            try {
+                src[i] = new File(files[i]);
+                images[i] = ImageIO.read(src[i]);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            int width = images[i].getWidth();
+            int height = images[i].getHeight();
+            ImageArrays[i] = new int[width * height];
+            ImageArrays[i] = images[i].getRGB(0, 0, width, height, ImageArrays[i], 0, width);
+        }
+        int newHeight = 0;
+        int newWidth = 0;
+        for (int i = 0; i < images.length; i++) {
+            // 横向
+            if (type == 1) {
+                newHeight = newHeight > images[i].getHeight() ? newHeight : images[i].getHeight();
+                newWidth += images[i].getWidth();
+            } else if (type == 2) {// 纵向
+                newWidth = newWidth > images[i].getWidth() ? newWidth : images[i].getWidth();
+                newHeight += images[i].getHeight();
+            }
+        }
+        if (type == 1 && newWidth < 1) {
+            return;
+        }
+        if (type == 2 && newHeight < 1) {
+            return;
+        }
+        // 生成新图片
+        try {
+            BufferedImage ImageNew = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            int height_i = 0;
+            int width_i = 0;
+            for (int i = 0; i < images.length; i++) {
+                if (type == 1) {
+                    ImageNew.setRGB(width_i, 0, images[i].getWidth(), newHeight, ImageArrays[i], 0,
+                            images[i].getWidth());
+                    width_i += images[i].getWidth();
+                } else if (type == 2) {
+                    ImageNew.setRGB(0, height_i, newWidth, images[i].getHeight(), ImageArrays[i], 0, newWidth);
+                    height_i += images[i].getHeight();
+                }
+            }
+            //输出想要的图片
+            ImageIO.write(ImageNew, targetFile.split("\\.")[1], new File(targetFile));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
